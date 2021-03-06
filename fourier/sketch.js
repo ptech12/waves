0d2e830c9b9bb1d295cc84894dc81527d5827acd
Prasanth A R <raviprasanth45@gmail.com>
2021-03-06 18:25:31 +0530

    Completed, Doing some tweaks

---
+++ fourier/sketch.js
@@ -0,0 +1,57 @@
+let time = 0;
+let waves = []
+let slider;
+let timeSlider;
+function setup() {
+    createCanvas(600, 400);
+    slider = createSlider(1, 100, 1, 1);
+    timeSlider = createSlider(0.01, 1, 0.05, 0.01);
+}
+let x, y;
+let n;
+// (4  * sin * n * time) / PI
+function draw()
+{
+    background(0);
+    x = 0;
+    y = 0;
+    translate(200, 200);
+
+   for(let i = 0; i < slider.value(); ++i)
+   {
+    let prevx = x;
+    let prevy = y;
+    n = i * 2 + 1; 
+    let r = 75 * (4 / (n * PI));
+    x += r * cos(n * time);
+    y += r * sin(n * time);
+   
+    noFill();
+    stroke(255, 100);
+    ellipse(prevx, prevy, r*2);
+
+    ellipse(x, y, 8);
+    stroke(255);
+    line(prevx, prevy, x, y);
+   
+
+
+   
+   }
+   noFill();
+   waves.unshift(y);
+
+    translate(200, 0);
+    line(x-200, y, 0, waves[0]);
+
+    beginShape();
+    for(let i=0; i<= waves.length; ++i)
+    {
+        vertex(i, waves[i]);   
+    }
+    endShape();
+
+    time += timeSlider.value();
+    if(waves.length >= 256)
+        waves.pop();
+}
